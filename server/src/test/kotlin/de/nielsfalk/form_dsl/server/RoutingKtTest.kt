4a3f8f2aba707241421b31c50fbc592b6b1a1d92
Niels Falk <niels@nielsfalk.de>
2024-02-02 00:15:02 +0100

    server: test routing with testForm

--- server/src/test/kotlin/de/nielsfalk/form_dsl/server/RoutingKtTest.kt
+++ server/src/test/kotlin/de/nielsfalk/form_dsl/server/RoutingKtTest.kt
@@ -8,12 +8,13 @@ import de.nielsfalk.form_dsl.server.db.findById
 import de.nielsfalk.form_dsl.server.db.lazyGetCollection
 import de.nielsfalk.form_dsl.server.plugins.configureSerialization
 import de.nielsfalk.formdsl.dsl.Form
-import de.nielsfalk.formdsl.forms.allForms
-import de.nielsfalk.formdsl.forms.noodle
-import de.nielsfalk.formdsl.forms.noodleId
+import de.nielsfalk.formdsl.dsl.form
 import de.nielsfalk.formdsl.misc.FormData
-import de.nielsfalk.formdsl.misc.FormDataValue.*
+import de.nielsfalk.formdsl.misc.FormDataValue.ListValue
+import de.nielsfalk.formdsl.misc.FormDataValue.LocalDateTimeValue
+import de.nielsfalk.formdsl.misc.FormDataValue.StringValue
 import de.nielsfalk.formdsl.misc.FormsList
+import de.nielsfalk.formdsl.misc.FormsListItem
 import de.nielsfalk.jsonUtil.defaultJson
 import getPlatform
 import io.kotest.core.Tag
@@ -22,21 +23,25 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.core.test.EnabledIf
 import io.kotest.core.test.TestCaseSeverityLevel
 import io.kotest.core.test.TestScope
-import io.kotest.matchers.collections.contain
-import io.kotest.matchers.should
+import io.kotest.matchers.equals.shouldBeEqual
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.string.shouldStartWith
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.put
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.HttpStatusCode.Companion.Conflict
 import io.ktor.http.HttpStatusCode.Companion.OK
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.testing.*
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.testing.testApplication
 import kotlinx.datetime.Instant
 import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.json.Json
@@ -48,17 +53,24 @@ class RoutingKtTest : FreeSpec({
         client.get("/forms").apply {
 
             status shouldBe OK
-            body<FormsList>().forms.map { it.title } should contain("a Noodle survey")
+            body<FormsList>() shouldBeEqual FormsList(
+                listOf(
+                    FormsListItem(
+                        testFormId,
+                        "a Noodle survey"
+                    )
+                )
+            )
         }
     }
 
     "GET /form/{id}".withTestApp {
-        val id = noodleId
+        val id = testFormId
 
         client.get("/forms/$id").apply {
 
             status shouldBe OK
-            body<Form>() shouldBe allForms.first { it.id.hexString == id }
+            body<Form>() shouldBe testForm
         }
     }
 
@@ -77,7 +89,7 @@ class RoutingKtTest : FreeSpec({
     var formDataId: String? = null
 
     "POST /forms/{formId}/data".withTestApp {
-        client.post("/forms/$noodleId/data") {
+        client.post("/forms/$testFormId/data") {
             setBody(formData)
             contentType(ContentType.Application.Json)
         }.apply {
@@ -85,10 +97,10 @@ class RoutingKtTest : FreeSpec({
 
             status shouldBe HttpStatusCode.Created
             body<FormData>() shouldBe formData
-            headers["Location"] shouldStartWith "/forms/$noodleId/data/"
+            headers["Location"] shouldStartWith "/forms/$testFormId/data/"
             collection.findById(ObjectId(formDataId!!)) shouldBe FormDataEntity(
                 id = ObjectId(formDataId),
-                formId = ObjectId(noodleId),
+                formId = ObjectId(testFormId),
                 values = formData.values,
                 platform = getPlatform().name
             )
@@ -96,7 +108,7 @@ class RoutingKtTest : FreeSpec({
     }
 
     "GET /forms/{formId}/data/{formDataId}".withTestApp {
-        client.get("/forms/$noodleId/data/$formDataId").apply {
+        client.get("/forms/$testFormId/data/$formDataId").apply {
 
             status shouldBe OK
             body<FormData>() shouldBe formData
@@ -107,7 +119,7 @@ class RoutingKtTest : FreeSpec({
         val updateData =
             formData.copy(values = formData.values + ("name" to StringValue("Niels J. Falk")))
 
-        client.put("/forms/$noodleId/data/$formDataId") {
+        client.put("/forms/$testFormId/data/$formDataId") {
             setBody(updateData)
             contentType(ContentType.Application.Json)
         }.apply {
@@ -116,7 +128,7 @@ class RoutingKtTest : FreeSpec({
             body<FormData>() shouldBe updateData.copy(version = 1)
             collection.findById(ObjectId(formDataId!!)) shouldBe FormDataEntity(
                 id = ObjectId(formDataId),
-                formId = ObjectId(noodleId),
+                formId = ObjectId(testFormId),
                 values = updateData.values,
                 platform = getPlatform().name,
                 version = 1
@@ -127,7 +139,7 @@ class RoutingKtTest : FreeSpec({
     "PUT /forms/{formId}/data/{formDataId} outdated".withTestApp {
         val updateData = formData.copy(values = mapOf("foo" to StringValue("outdated")))
 
-        client.put("/forms/$noodleId/data/$formDataId") {
+        client.put("/forms/$testFormId/data/$formDataId") {
             setBody(updateData)
             contentType(ContentType.Application.Json)
         }.apply {
@@ -138,7 +150,7 @@ class RoutingKtTest : FreeSpec({
     }
 
     "GET /forms/{formId}/evaluation".withTestApp {
-        client.get("/forms/$noodleId/evaluation")
+        client.get("/forms/$testFormId/evaluation")
             .apply {
 
                 status shouldBe OK
@@ -147,7 +159,7 @@ class RoutingKtTest : FreeSpec({
                     shouldContain("<th>name Please enter your name</th>")
                     shouldContain("<th>noodleSection-selectMulti0 Do you have time on</th>")
                     shouldContain("<th>platform</th>")
-                    shouldContain("<td>${Instant.fromEpochSeconds(ObjectId(formDataId).timestamp.toLong()).toString()}</td>")
+                    shouldContain("<td>${Instant.fromEpochSeconds(ObjectId(formDataId).timestamp.toLong())}</td>")
                     shouldContain("<td>Niels J. Falk</td>")
                     shouldContain("<p>2024-08-30T18:43</p>")
                     shouldContain("<p>2024-08-31T18:43</p>")
@@ -156,6 +168,29 @@ class RoutingKtTest : FreeSpec({
             }
     }
 })
+
+const val testFormId = "65bc236ecbe35e78d678cfd5"
+
+val testForm = form {
+    id = testFormId
+    title = "a Noodle survey"
+    label("Noodle is something like Doodle")
+    section {
+        id = "noodleSection"
+        textInput {
+            id = "name"
+            description = "Please enter your name"
+            placehoder = "Name"
+        }
+
+        selectMulti {
+            description = "Do you have time on"
+            option("2024-08-30T18:43".toLocalDateTime())
+            option("2024-08-31T18:43".toLocalDateTime())
+        }
+    }
+}
+
 
 context (FreeSpec)
 private fun String.withTestApp(
@@ -193,7 +228,7 @@ private fun String.withTestApp(
         val service = FormService(
             database,
             collection,
-            allForms
+            allForms = listOf(testForm)
         )
 
         testApplication {
